0297852aa4307f6d5ca0722609a6389b94a21ca2
bhushank <bhushank.bwd@gmail.com>
2024-01-14 21:46:40 +0530

    route added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import { Provider } from "react-redux";
 import "./App.css";
 import store from "./utils/store";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Account from "./components/Account";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="text-3xl font-bold underline">Talk</div>
+      <RouterProvider router={appRouter}></RouterProvider>
     </Provider>
   );
 }
-
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Account />,
+  },
+  {
+    path: "/chat",
+    element: <Chat />,
+  },
+]);
 export default App;
