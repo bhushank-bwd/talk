e5ea9bab9520005b3b48a20538afed3c319e362d
bhushank <bhushank.bwd@gmail.com>
2024-01-16 11:35:07 +0530

    logout button added

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,30 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { auth } from "../utils/fireBase";
+import { useNavigate } from "react-router-dom";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const navigate = useNavigate();
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+  return (
+    <div className="flex justify-between">
+      <span>Logo</span>
+      <button
+        className="p-2 text-xl cursor-pointer bg-red-600 text-white m-2 rounded-md"
+        onClick={logout}
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Chat;
